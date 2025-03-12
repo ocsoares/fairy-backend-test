4e5247bd8f5596ce9151a7c9c45252bc9c59ff08
Cauã Soares <casoares016@gmail.com>
2025-03-11 22:51:10 -0300

    chore: remove useless older files

--- src/entity/ProductEntity.ts
+++
@@ -1,9 +0,0 @@
-export class ProductEntity {
-    constructor(
-        public readonly name: string,
-        public readonly brand: string,
-        public readonly cnpj: string,
-        public readonly price: number,
-        public readonly hash: string,
-    ) {}
-}
--- src/modules/product/responses/ProductResponse.ts
+++
@@ -1,7 +0,0 @@
-export class ProductResponse {
-    constructor(
-        public readonly name: string,
-        public readonly brand: string,
-        public readonly price: number,
-    ) {}
-}
