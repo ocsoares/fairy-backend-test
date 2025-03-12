8f40e1783bc9e14caeed131f33686c09ffe8c509
Cauã Soares <casoares016@gmail.com>
2025-03-11 22:54:11 -0300

    chore: update prisma schema

---
+++ src/repositories/implementations/prisma/migrations/20250312011156_add_status_enum/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('pending', 'in_progress', 'completed');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "TaskStatus" NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
